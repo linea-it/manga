14d220ecc9804678b13db040035fbac823ee42f8
Matheus Allein <mtsallein@gmail.com>
2020-07-07 15:49:25 -0300

    Adding histograms

--- frontend/src/components/Plot/Age.js
+++ frontend/src/components/Plot/Age.js
@@ -4,62 +4,62 @@ import Plotly from 'plotly.js';
 import createPlotlyComponent from 'react-plotly.js/factory';
 
 function Age({ data, height }) {
-
   const Plot = createPlotlyComponent(Plotly);
 
   return (
     <Plot
       data={[
-      {
-        x: data.x,
-        y: data.y,
-        name: '∑𝑥ⱼ',
-        type: "bar",
-        width: 0.8,
-      },
-      {
-        x: data.x,
-        y: data.m,
-        name: '∑μⱼ',
-        type: "bar",
-        width: 0.2,
-        marker: {
-          opacity: 0.85,
-        }
-      }
+        {
+          x: data.x,
+          y: data.y,
+          name: '∑𝑥ⱼ',
+          type: 'bar',
+          width: 0.8,
+        },
+        {
+          x: data.x,
+          y: data.m,
+          name: '∑μⱼ',
+          type: 'bar',
+          width: 0.2,
+          marker: {
+            opacity: 0.85,
+          },
+        },
       ]}
       layout={{
-        height: height,
+        height,
         margin: {
           t: 30,
           b: 30,
-          r: 20
+          r: 20,
         },
         xaxis: {
           title: 'log(Age)',
         },
         yaxis: {
           title: '%',
+          range: [0, 100],
         },
         hovermode: 'closest',
         autosize: true,
       }}
     />
-  )
+  );
 }
 
 Age.propTypes = {
   data: PropTypes.shape({
     x: PropTypes.arrayOf(
-      PropTypes.number
+      PropTypes.number,
     ),
     y: PropTypes.arrayOf(
-      PropTypes.number
+      PropTypes.number,
     ),
     m: PropTypes.arrayOf(
-      PropTypes.number
+      PropTypes.number,
     ),
-  }).isRequired
-}
+  }).isRequired,
+};
 
 export default Age;
