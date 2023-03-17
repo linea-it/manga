6344a9c2b95cd249fe0b4a97ab1423d7868a5d87
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-03-17 12:03:28 -0300

    Docker-compose with separated celery services

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,45 +1,36 @@
-#!/bin/sh
-cd /usr/src/app/
-pwd
-YELLOW='\033[00;33m'
-NO_COLOR='\033[0m'
+#!/bin/bash
 
-# Se nao tiver o manage.py e a primeira vez que o container e executado, apenas abre o terminal.
-if [ -e manage.py ]
-then
-    # # Usar o server do django so para desenvolvimento
-    # echo "Running Django with ${YELLOW} DEVELOPMENT SERVER ${NO_COLOR}, for production use Gunicorn."
+# if any of the commands in your code fails for any reason, the entire script fails
+set -o errexit
+# fail exit if one of your pipe command fails
+set -o pipefail
+# exits if any of your variables is not set
+set -o nounset
 
-    echo "Running Migrate to apply changes in database"
-    python manage.py migrate
+postgres_ready() {
+python << END
+import sys
 
-    echo "Running Collect Statics"
-    python manage.py collectstatic --clear --noinput --verbosity 0
+import psycopg2
 
-    # # Start Celery Workers
-    # celery worker --workdir /app --app manga -l info &> /log/celery.log  &
+try:
+    psycopg2.connect(
+        dbname="${SQL_DATABASE}",
+        user="${SQL_USER}",
+        password="${SQL_PASSWORD}",
+        host="${SQL_HOST}",
+        port="${SQL_PORT}",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
 
-    # # Start Celery Beat
-    # celery worker --workdir /app --app manga -l info --beat &> /log/celery_beat.log  &
+END
+}
+until postgres_ready; do
+    >&2 echo 'Waiting for PostgreSQL to become available...'
+    sleep 1
+done
+>&2 echo 'PostgreSQL is available'
 
-    # uWSGI para servir o app e ter compatibilidade com Shibboleth
-    # https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
-    # TODO: Em produção não é recomendado o auto reload. utilizar uma variavel de ambiente para ligar ou desligar esta opção.
-    echo "${YELLOW}Running Django with uWSGI.${NO_COLOR}"
-    # 
-    uwsgi \
-        --socket 0.0.0.0:8000 \
-        --wsgi-file /usr/src/app/manga/wsgi.py \
-        --module manga.wsgi:application \
-        --buffer-size=32768 \
-        --processes=4 \
-        --threads=2 \
-        --http-timeout=120 \
-        --static-map /django_static=/usr/src/app/django_static \
-        --py-autoreload=1
-else
-    /bin/bash
-fi
-
-
-
+exec "$@"
--- backend/manga/settings.py
+++ backend/manga/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "development_secret_key_please_generate_a_unique_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=False))
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
