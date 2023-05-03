f344b3d81c4ab48d8562addb2e80228c39e1f521
Cristiano Singulani <crisingulani@gmail.com>
2023-05-03 09:53:02 -0300

    Fixed bug in plots

--- backend/galaxy/views.py
+++ backend/galaxy/views.py
@@ -312,9 +312,9 @@ class ImageViewSet(viewsets.ModelViewSet):
         synt, lamb2 = mclass().synt_by_position(
             megacube, int(params['x']), int(params['y']))
         result = dict({
-            'flux': flux.tolist(fill_value=None),
-            'lamb': lamb.tolist(fill_value=None),
-            'synt': synt.tolist(fill_value=None),
+            'flux': flux.tolist(),
+            'lamb': lamb.tolist(),
+            'synt': synt.tolist(),
         })
 
         return Response(result)
@@ -466,7 +466,7 @@ class ImageViewSet(viewsets.ModelViewSet):
         })
 
         for label in df.columns:
-            data[label] = df[label].tolist(fill_value=None)        
+            data[label] = df[label].tolist()        
         return Response(data)
 
     @action(detail=True, methods=['get'])
