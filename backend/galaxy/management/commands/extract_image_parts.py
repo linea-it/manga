4c44a640ff07fafd868a436762f16c0400265e7d
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-07-28 16:36:29 -0300

    Fix extract_image_parts

--- backend/galaxy/management/commands/extract_image_parts.py
+++ backend/galaxy/management/commands/extract_image_parts.py
@@ -9,12 +9,20 @@ from galaxy.models import Image
 
 from manga.megacubo_utils import get_megacube_parts_root_path
 from manga.megacube import MangaMegacube
-
+import json
+import requests
+import shutil
 
 class Command(BaseCommand):
     help = 'Extracting image parts to separate files to gain performance'
 
     def add_arguments(self, parser):
+
+        parser.add_argument(
+            '--name',
+            dest='name',
+            help='Megacube name',
+        )
 
         parser.add_argument(
             '--limit',
@@ -42,6 +50,8 @@ class Command(BaseCommand):
         if kwargs['force_overwrite']:
             # Todos os objetos independente de já ter sido executado.
             objs = Image.objects.all()
+        elif kwargs['name']:
+            objs = Image.objects.filter(megacube=kwargs['name'])
         else:
             # Apenas objetos que ainda não foram executados.
             objs = Image.objects.filter(had_parts_extracted=False)
@@ -54,10 +64,11 @@ class Command(BaseCommand):
         for obj in objs:
             title = " [%s/%s] " % (current, len(objs))
             self.stdout.write(title.center(80, '-'))
-
-            exec_time = self.process_single_object(obj)
-            exec_times.append(exec_time)
-            current += 1
+            
+            if obj.path != None:
+                exec_time = self.process_single_object(obj)
+                exec_times.append(exec_time)
+                current += 1
 
             self.stdout.write("".ljust(80, '-'))
 
@@ -97,14 +108,56 @@ class Command(BaseCommand):
         #  Extrair o megacubo bz2 -> fits
         cube.extract_bz2()
         cube.extract_megacube_parts(parts_path)
-        cube.compress_bz2()
+        # cube.compress_bz2()
 
         obj.had_parts_extracted = True
         obj.save()
 
+        self.download_images(obj)
+
         t1 = datetime.now()
         tdelta = t1 - t0
         self.stdout.write('Execution Time: [%s]' %
-                          humanize.precisedelta(tdelta))
+                        humanize.precisedelta(tdelta))
 
         return tdelta.total_seconds()
+
+
+    def download_images(self, obj):
+        """
+            It downloads the SDSS Image by its RA and Dec
+            for each image and save them in the path
+            /images/megacube_parts/megacube_{JOB_ID}/sdss_image.jpg
+        """
+        self.stdout.write('Downloading SDSS Image [%s]' % str(obj.megacube))
+        # Object directory in Images Megacube Parts.
+        parts_path = get_megacube_parts_root_path().joinpath(obj.folder_name)
+        ra = obj.objra
+        dec = obj.objdec
+
+        # Set up the image URL and filename
+        image_url = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Chart.Image&ra=%s&dec=%s&scale=0.099515875&width=512&height=512&opt=G&query=" % (ra, dec)
+        filename = 'sdss_image.jpg'
+
+        # Open the url image, set stream to True, this will return the stream content.
+        r = requests.get(image_url, stream=True)
+
+        # Check if the image was retrieved successfully
+        if r.status_code == 200:
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            r.raw.decode_content = True
+
+            filepath = Path(parts_path).joinpath(filename)
+            if filepath.exists():
+                filepath.unlink()
+
+            # Open a local file with wb ( write binary ) permission.
+            with open(filepath, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)  
+            self.stdout.write('Finished Download SDSS Images!')                
+        else:
+            self.stdout.write(
+                'SDSS Image [%s] could not be retrieved' % str(obj.megacube))
+
+
+
