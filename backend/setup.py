d517fa7b3d780804388510d5fa71e3ec72070fb3
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-09-18 13:11:30 -0300

    feat(Test): Test Commitizen

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="manga",
-    version="1.0.1",
+    version="1.0.0",
     packages=find_packages(),
     scripts=["manage.py"],
     license="MIT",
