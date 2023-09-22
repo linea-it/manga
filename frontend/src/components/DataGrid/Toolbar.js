4d2229a48dbf601cc87ffe6a8379620684029175
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-09-22 15:06:08 -0300

    feat(MeanTableDownload): added information about mean table file before download
    Closed #141

--- frontend/src/components/DataGrid/Toolbar.js
+++ frontend/src/components/DataGrid/Toolbar.js
@@ -7,16 +7,22 @@ import {
 } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
+import MeanTableDownload from '../MeanTableDownload';
 
 
-const handleDownload = () => {
-  const link = document.createElement("a");
-  link.download = `megacube_mean_properties_table_Riffel_2023.fits.tar.gz`;
-  link.href = "/table/megacube_mean_properties_table_Riffel_2023.fits.tar.gz";
-  link.click();
-};
+// const handleDownload = () => {
+//   const link = document.createElement("a");
+//   link.download = `megacube_mean_properties_table_Riffel_2023.fits.tar.gz`;
+//   link.href = "/table/megacube_mean_properties_table_Riffel_2023.fits.tar.gz";
+//   link.click();
+// };
 
 function CustomToolbar() {
+
+  const [downloadIsOpen, setDownloadIsOpen] = React.useState(false)
+
+  const handleDownload = () => setDownloadIsOpen(!downloadIsOpen);
+
   return (
     <GridToolbarContainer>
       <GridToolbarQuickFilter debounceMs={600} />
@@ -25,6 +31,7 @@ function CustomToolbar() {
       <Button
         onClick={handleDownload}
         startIcon={<DownloadIcon />}>Download mean properties file</Button>
+      <MeanTableDownload open={downloadIsOpen} onClose={handleDownload} />
     </GridToolbarContainer>
   );
 }
